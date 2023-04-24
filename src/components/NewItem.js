96dbfe19ccec4780f9210e3db806ec1ddf4d0ca9
cmtrc <comtruice@gmail.com@>
2023-04-24 20:20:43 +0200

    add functionality to newItem component

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,38 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NewItem = () => {
-    return (
-      <div>
-        <label>
-          Description: <input name="myInput" />
-        </label>
-        <p>
-          Category:
-          <label>
-            <input type="radio" name="myRadio" value="option1" />
-            Plumbing
-          </label>
-          <label>
-            <input type="radio" name="myRadio" value="option2" />
-            Electrical
-          </label>
-          <label>
-            <input type="radio" name="myRadio" value="option3" />
-            Framing
-          </label>
-        </p>
-        <label for="due-date">Due date:</label>
-  
-        <input
-          type="date"
-          id="due-date"
-          name="due-date"
-          value={"2023-04-25"}
-          min={"2023-04-24"}
-          max="2023-05-31"
-        />
-      </div>
-    );
+const NewItem = ({ addItem }) => {
+  const [item, setItem] = useState({
+    description: "",
+    category: "plumbing",
+    dueDate: "2023-04-25",
+  });
+
+  const handleChange = (event) => {
+    setItem({ ...item, [event.target.name]: event.target.value });
   };
-  
-  export default NewItem;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    addItem(item);
+
+    setItem({ description: "", category: "", dueDate: "" });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Add new item</h1>
+      <label>
+        Description:
+        <input
+          name="description"
+          value={item.description}
+          onChange={handleChange}
+        />
+      </label>
+      <p>
+        Category:
+        <label>
+          <input
+            checked={item.category === "plumbing"}
+            type="radio"
+            name="category"
+            value="plumbing"
+            onChange={handleChange}
+          />
+          Plumbing
+        </label>
+        <label>
+          <input
+            checked={item.category === "electrical"}
+            type="radio"
+            name="category"
+            value="electrical"
+            onChange={handleChange}
+          />
+          Electrical
+        </label>
+        <label>
+          <input
+            checked={item.category === "framing"}
+            type="radio"
+            name="category"
+            value="framing"
+            onChange={handleChange}
+          />
+          Framing
+        </label>
+      </p>
+      <label for="dueDate">Due date:</label>
+      <input
+        type="date"
+        id="dueDate"
+        name="dueDate"
+        value={item.dueDate}
+        min="2023-04-25"
+        max="2023-05-30"
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default NewItem;
